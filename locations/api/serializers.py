c68dbb5a2e9e5204ca3fc0bd8af58725ce8b92cf
saifullah3396 <Saifullah3396@gmail.com>
2020-10-12 03:52:39 +0500

    Updated serializer to give both coordinate frame and image url.

--- locations/api/serializers.py
+++ locations/api/serializers.py
@@ -15,6 +15,17 @@ class FloorSerializer(serializers.ModelSerializer):
 
 
 class BlockSerializer(serializers.ModelSerializer):
+    floor = FloorSerializer()
+    floor_map_url = serializers.SerializerMethodField()
+
     class Meta:
-        model = Floor
-        fields = ('id', 'number', 'floor')
+        model = Block
+        fields = ('id', 'name', 'coordinate_frame', 'floor', 'floor_map_url')
+
+    def get_floor_map_url(self, block):
+        request = self.context.get('request')
+        if block.floor_map and hasattr(block.floor_map, 'url'):
+            floor_map_url = block.floor_map.url
+            return request.build_absolute_uri(floor_map_url)
+        else:
+            None
