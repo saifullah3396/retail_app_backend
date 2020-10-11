357b47c1a39f198dc13c029500cf23e84fcf0c76
saifullah3396 <Saifullah3396@gmail.com>
2020-10-11 20:59:54 +0500

    Fixed fields for organization.

--- organizations/models.py
+++ organizations/models.py
@@ -7,10 +7,15 @@ class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     # organization title
-    title = models.CharField(max_length=120, default="Unknown")
+    title = models.CharField(max_length=120, blank="Unknown", unique=True)
 
     # organization description
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
 
     def __str__(self):
         return self.title
