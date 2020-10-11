e89f82c0422b7e5466256b57e6c6010ef69839d1
saifullah3396 <Saifullah3396@gmail.com>
2020-10-12 03:54:49 +0500

    Added cameras and media path urls.

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('organizations/api/', include('organizations.api.urls')),
     path('locations/api/', include('locations.api.urls')),
-]
+    path('cameras/api/', include('cameras.api.urls')),
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
