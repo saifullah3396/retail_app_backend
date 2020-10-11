fe7b7fdf8e32f1528d38329bc3e63331bc24de9c
saifullah3396 <Saifullah3396@gmail.com>
2020-10-12 03:51:27 +0500

    Updated permissions.

--- organizations/api/views.py
+++ organizations/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from ..models import Organization, SubOrganization
 from .serializers import OrganizationSerializer, SubOrganizationSerializer
 
@@ -6,18 +7,22 @@ from .serializers import OrganizationSerializer, SubOrganizationSerializer
 class OrganizationListView(ListAPIView):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+    permission_classes = (permissions.AllowAny, )
 
 
 class OrganizationDetailView(RetrieveAPIView):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+    permission_classes = (permissions.AllowAny, )
 
 
 class SubOrganizationListView(ListAPIView):
     queryset = SubOrganization.objects.all()
     serializer_class = SubOrganizationSerializer
+    permission_classes = (permissions.AllowAny, )
 
 
 class SubOrganizationDetailView(RetrieveAPIView):
     queryset = SubOrganization.objects.all()
     serializer_class = SubOrganizationSerializer
+    permission_classes = (permissions.AllowAny, )
