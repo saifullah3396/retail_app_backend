f893af8ecb40aceac63786ac7524bdd11b92cbc6
saifullah3396 <Saifullah3396@gmail.com>
2020-10-11 21:00:12 +0500

    Added sub-organization to models.

--- organizations/models.py
+++ organizations/models.py
@@ -16,6 +16,21 @@ class Organization(models.Model):
         return self.title
 
 
+class SubOrganization(models.Model):
+    # generate unique uuid for each sub-organization
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # sub-organization title
+    title = models.CharField(max_length=120, default="Unknown", unique=True)
+
+    # sub-organization description
+    desc = models.TextField(blank=True)
+
+    # organization with which this sub-organization is associated
+    organization = models.ForeignKey(
+        'organizations.Organization',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
