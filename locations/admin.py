dbfe1ba8871c60b1f27991bfc994d5ea6314fe09
saifullah3396 <Saifullah3396@gmail.com>
2020-10-11 21:03:52 +0500

    Added locations to admin.

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Location, Floor, Block
 
-# Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Floor)
+admin.site.register(Block)
