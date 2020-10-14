b2e11047d9cafb99f93c323bfe1e1147497dc219
saifullah3396 <saifullah3396@gmail.com>
2020-10-15 00:10:49 +0500

    Added fitler views url.

--- cameras/api/urls.py
+++ cameras/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CameraListView, CameraDetailView
+from .views import CameraListView, CameraDetailView, CameraFilterListView, \
+    CameraFilterDetailView
 
 urlpatterns = [
     path('', CameraListView.as_view()),
     path('<pk>', CameraDetailView.as_view()),
+    path('filter/', CameraFilterListView.as_view()),
+    path('filter/<pk>', CameraFilterDetailView.as_view()),
 ]
