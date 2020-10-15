43bcf29efb00cb7af82ca77deaf23a56a3a22470
saifullah3396 <saifullah3396@gmail.com>
2020-10-15 13:17:56 +0500

    Updated api patterns and added rest_registration for accounts.

--- backend/urls.py
+++ backend/urls.py
@@ -3,10 +3,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from .settings import MEDIA_URL, MEDIA_ROOT
 
+api_urlpatterns = [
+    path('accounts/', include('rest_registration.api.urls')),
+    path('organizations/', include('organizations.api.urls')),
+    path('locations/', include('locations.api.urls')),
+    path('cameras/', include('cameras.api.urls')),
+]
+
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('organizations/api/', include('organizations.api.urls')),
-    path('locations/api/', include('locations.api.urls')),
-    path('cameras/api/', include('cameras.api.urls')),
+    path('api/v1/', include(api_urlpatterns)),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
