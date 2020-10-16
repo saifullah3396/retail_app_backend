f9a9378bb259e7afa02d6daa98e59fc185e2e8de
Saifullah3396 <Saifullah3396@gmail.com>
2020-10-16 21:31:45 +0500

    Updated urls for newly added rest auth and django allauth.

--- backend/urls.py
+++ backend/urls.py
@@ -4,14 +4,15 @@ from django.conf.urls.static import static
 from .settings import MEDIA_URL, MEDIA_ROOT
 
 api_urlpatterns = [
-    path('accounts/', include('rest_registration.api.urls')),
     path('organizations/', include('organizations.api.urls')),
     path('locations/', include('locations.api.urls')),
     path('cameras/', include('cameras.api.urls')),
 ]
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('rest_auth.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_urlpatterns)),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
