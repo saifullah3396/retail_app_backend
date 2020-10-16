4bb54c261e8b968baf0af9e125f37bdc30a653c8
Saifullah3396 <Saifullah3396@gmail.com>
2020-10-16 21:32:01 +0500

    Updated post requests for rest-auth.

--- scripts/login_post.sh
+++ scripts/login_post.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" --data @login_post.json http://127.0.0.1:8000/api/v1/accounts/login/
+curl -H "Content-Type: application/json" --data @login_post.json http://127.0.0.1:8000/accounts/login/
--- scripts/register_post.sh
+++ scripts/register_post.sh
@@ -1,2 +1,2 @@
 #curl -X POST -d "@register_post.txt" http://127.0.0.1:8000/api/v1/accounts/register/
-curl -H "Content-Type: application/json" --data @register_post.json http://127.0.0.1:8000/api/v1/accounts/register/
+curl -H "Content-Type: application/json" --data @register_post.json http://127.0.0.1:8000/accounts/registration/
