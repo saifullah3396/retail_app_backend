42cf8f06581ae5d34bee27139218d53d779bb4d1
saifullah3396 <saifullah3396@gmail.com>
2020-10-17 23:06:45 +0500

    Changed title to name.

--- organizations/models.py
+++ organizations/models.py
@@ -6,22 +6,22 @@ class Organization(models.Model):
     # generate unique uuid for each organization
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # organization title
-    title = models.CharField(max_length=120, default="Unknown", unique=True)
+    # organization name
+    name = models.CharField(max_length=120, default="Unknown", unique=True)
 
     # organization description
     desc = models.TextField(blank=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class SubOrganization(models.Model):
     # generate unique uuid for each sub-organization
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # sub-organization title
-    title = models.CharField(max_length=120, default="Unknown", unique=True)
+    # sub-organization name
+    name = models.CharField(max_length=120, default="Unknown", unique=True)
 
     # sub-organization description
     desc = models.TextField(blank=True)
@@ -33,4 +33,4 @@ class SubOrganization(models.Model):
     )
 
     def __str__(self):
-        return self.title
+        return self.name
