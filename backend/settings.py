cae5ab8ff68d305a257986c5ea50055c7d2e8ba5
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-12 16:38:55 +0500

    Added JWT authentication to user registration in rest-auth.

--- backend/settings.py
+++ backend/settings.py
@@ -201,8 +201,43 @@ REST_AUTH_REGISTER_PERMISSION_CLASSES = [
 REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'users.serializers.AppRegisterSerializer',
 }
-REGISTER_AVAILABLE_GROUPS = [
-    'organization_admin',
-    'sub_organization_admin',
-    'employee']
+
+REST_AUTH_SERIALIZERS = {
+    'JWT_SERIALIZER':
+        'users.serializers.JWTSerializer',
+}
 ACCOUNT_ADAPTER = 'users.adapter.AppAccountAdapter'
+REST_USE_JWT = True
+
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+
+}
