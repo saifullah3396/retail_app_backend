4a94b2e55deda70a3324c4ac066d5b665d57f22b
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-12 16:41:15 +0500

    Fixed url.

--- backend/settings.py
+++ backend/settings.py
@@ -27,10 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000'
-# )
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
 
 ALLOWED_HOSTS = []
 
