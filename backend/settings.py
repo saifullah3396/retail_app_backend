e23f8f8991ab19fd6cbeac33bfc91be36eb7955d
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-12 16:40:54 +0500

    Added JWT as default.

--- backend/settings.py
+++ backend/settings.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
