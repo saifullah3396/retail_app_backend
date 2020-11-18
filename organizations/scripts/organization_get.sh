b68988b87df2f2488b100d0a32c17bab3f05efde
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-18 09:15:50 +0500

    Added some scripts for testing organization api calls.

--- organizations/scripts/organization_get.sh
+++ organizations/scripts/organization_get.sh
@@ -1,1 +1,1 @@
-curl -X GET -H "Authorization: Token 454bc4b3f9626430d5c86bcaa17641e3cc8836bc" http://127.0.0.1:8000/api/v1/organizations/
+curl -X GET -H "Authorization: Token $1" http://127.0.0.1:8000/api/v1/organizations/admin/
---
+++ organizations/scripts/sub_organization_get.sh
@@ -0,0 +1,1 @@
+curl -X GET -H "Authorization: Token $1" http://127.0.0.1:8000/api/v1/organizations/admin/sub/
