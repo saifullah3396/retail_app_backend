51a86b7d062a7b52ffe76a5bd608767298f6f3c2
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-18 09:12:12 +0500

    Changed organization views to ListCreateView and RUDView. Also added pagination, ordering filters and search filters based on djangoFilterBackend.

--- organizations/api/views.py
+++ organizations/api/views.py
@@ -1,5 +1,9 @@
-from rest_framework import authentication, permissions
-from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveAPIView, CreateAPIView
+from rest_framework import permissions, pagination, filters, status
+from rest_framework_jwt import authentication
+from rest_framework.generics import \
+    GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Organization, SubOrganization
 from .serializers import AdminOnlyOrganizationSerializer, AdminOnlySubOrganizationSerializer
 from django.contrib.auth.mixins import UserPassesTestMixin
@@ -9,6 +13,50 @@ class PaginationConfig(pagination.PageNumberPagination):
     page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class OrganizationsListCreateView(ListCreateAPIView):
+    queryset = Organization.objects.all().order_by('name')
+    serializer_class = OrganizationSerializer
+    # permission_classes = (
+    #     permissions.IsAuthenticated, permissions.IsAdminUser)
+    authentication_classes = [authentication.JSONWebTokenAuthentication]
+    pagination_class = PaginationConfig
+    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
+    ordering_fields = ['id', 'name', 'desc']
+    filterset_fields = {
+        'name': ['exact', 'icontains'],
+    }
+
+
+class OrganizationsRUDView(RetrieveUpdateDestroyAPIView):
+    serializer_class = OrganizationSerializer
+    permission_classes = (
+        permissions.IsAuthenticated,
+        OrganizationsRUDPermissions)
+    authentication_classes = [authentication.JSONWebTokenAuthentication]
+    pagination_class = PaginationConfig
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['id', 'name', 'desc']
+    filterset_fields = {
+        'name': ['exact', 'icontains'],
+    }
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            # return all if it is super admin
+            queryset = \
+                Organization.objects.all().order_by('name')
+            return queryset
+        else:
+            # else can only be an organization admin according to permissions
+            # on the view. Return all sub-organizations under the users
+            # organization
+            queryset = \
+                Organization.objects.filter(
+                    id=user.organization.id).order_by('name')
+            return queryset
 
 
 
