3bd2caca05feb0f0901ac0a92fd3db034e044cc5
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-18 09:14:31 +0500

    Fixed names of serializers.

--- organizations/api/serializers.py
+++ organizations/api/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from ..models import Organization, SubOrganization
 
 
-class AdminOnlyOrganizationSerializer(serializers.ModelSerializer):
+class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
         fields = ('id', 'name', 'desc')
 
 
-class AdminOnlySubOrganizationSerializer(serializers.ModelSerializer):
+class SubOrganizationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = SubOrganization
         fields = ('id', 'name', 'desc', 'organization')
