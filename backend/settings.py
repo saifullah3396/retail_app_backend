2460eccefdb1c9d8d1733fd7bcaa69f38d4e8d8e
Saifullah3396 <Saifullah3396@gmail.com>
2020-11-19 17:48:34 +0500

    Removed json renderer for easy view.

--- backend/settings.py
+++ backend/settings.py
@@ -163,9 +163,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
