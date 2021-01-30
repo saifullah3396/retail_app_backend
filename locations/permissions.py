b868aa75fface6f0857e0ebad1205af4fd4ede04
Saifullah <saifullah3396@gmail.com>
2021-01-30 22:48:47 +0500

    Refactor after adding core commands.

--- locations/permissions.py
+++ locations/permissions.py
@@ -1,5 +1,5 @@
 import copy
-from common.permissions import UserGroups, AppDjangoModelPermissions
+from core.permissions import UserGroups, AppDjangoModelPermissions
 from .models import Location
 
 USER_GROUP_PERMISSIONS = {
