e3888f1cdbe52d4b40b0fbfab6779512aa1f01fe
Saifullah <saifullah3396@gmail.com>
2021-01-30 20:46:16 +0500

    Moved permission base class to common.

--- organizations/permissions.py
+++ organizations/permissions.py
@@ -1,6 +1,5 @@
 import copy
-from rest_framework import permissions
-from backend.settings import UserGroups
+from common.permissions import UserGroups, AppDjangoModelPermissions
 from .models import Organization
 
 USER_GROUP_PERMISSIONS = {
@@ -11,25 +10,6 @@ USER_GROUP_PERMISSIONS = {
         Organization: ['view'],
     },
 }
-
-
-class AppDjangoModelPermissions(permissions.DjangoModelPermissions):
-
-    def _queryset(self, view):
-        assert hasattr(view, 'get_queryset') \
-            or getattr(view, 'queryset', None) is not None, (
-            'Cannot apply {} on a view that does not set '
-            '`.queryset` or have a `.get_queryset()` method.'
-        ).format(self.__class__.__name__)
-
-        queryset = getattr(view, 'queryset', None)
-        if queryset is None:
-            queryset = view.get_queryset()
-            assert queryset is not None, (
-                'The value of {0}.queryset and {0}.get_queryset() is None.'.format(
-                    view.__class__.__name__)
-            )
-        return queryset
 
 
 class OrganizationListCreateDestroyPermission(AppDjangoModelPermissions):
