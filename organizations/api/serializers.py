91bc9e0b13539fb47c6388c108572d886c931ac4
Saifullah <saifullah3396@gmail.com>
2021-01-30 19:43:34 +0500

    Removed sub organizations from serializers.

--- organizations/api/serializers.py
+++ organizations/api/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
-from ..models import Organization, SubOrganization
+from ..models import Organization
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Organization
-        fields = ('id', 'name', 'desc')
-
-
-class SubOrganizationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SubOrganization
-        fields = ('id', 'name', 'desc', 'organization')
+        fields = '__all__'
