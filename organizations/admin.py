af9f0a70ca639628658b7753f0c776ad7bad41d2
Saifullah <saifullah3396@gmail.com>
2021-01-30 19:43:15 +0500

    Removed sub organizations.

--- organizations/admin.py
+++ organizations/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import Organization, SubOrganization
+from .models import Organization
 
 admin.site.register(Organization)
-admin.site.register(SubOrganization)
