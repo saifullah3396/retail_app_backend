5c57291954211821b99b3bda1508d108c0f9a8cf
Saifullah <saifullah3396@gmail.com>
2021-01-30 19:43:28 +0500

    Removed sub organizations from urls.

--- organizations/api/urls.py
+++ organizations/api/urls.py
@@ -1,26 +1,14 @@
 from django.urls import path
-from .views import \
-    OrganizationsListCreateView, \
-    OrganizationsRUDView, \
-    SubOrganizationsListCreateView, \
-    SubOrganizationsRUDView
+from .views import *
 
 
 urlpatterns = [
     path(
         '',
-        OrganizationsListCreateView.as_view(),
+        OrganizationsListCreateDestroyView.as_view(),
         name='organizations_list_create'),
     path(
         '<pk>',
-        OrganizationsRUDView.as_view(),
+        OrganizationsRetrieveUpdateDestroyView.as_view(),
         name='organizations_rud'),
-    path(
-        'sub/',
-        SubOrganizationsListCreateView.as_view(),
-        name='sub_organizations_list_create'),
-    path(
-        'sub/<pk>',
-        SubOrganizationsRUDView.as_view(),
-        name='sub_organizations_rud'),
 ]
