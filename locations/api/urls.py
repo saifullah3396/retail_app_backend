d142c2ec91baac00820d8155f374d8e8b2e2b604
Saifullah <saifullah3396@gmail.com>
2021-01-30 22:50:22 +0500

    Simplified urls according to new views.

--- locations/api/urls.py
+++ locations/api/urls.py
@@ -1,28 +1,14 @@
 from django.urls import path
-from .views import AdminLocationListView, AdminLocationDetailView, \
-    AdminFloorListView, AdminFloorDetailView, AdminBlockListView, \
-    AdminBlockDetailView, LocationDetailsAppUserAccess
+from .views import *
+
 
 urlpatterns = [
-    # admin views
-    path('admin/', AdminLocationListView.as_view(), name='locations_admin'),
     path(
-        'admin/<pk>',
-        AdminLocationDetailView.as_view(),
-        name='locations_admin_detail'),
-    path('admin/floors/', AdminFloorListView.as_view(), name='floors_admin'),
-    path(
-        'admin/floors/<pk>',
-        AdminFloorDetailView.as_view(),
-        name='floors_admin_detail'),
-    path('admin/blocks/', AdminBlockListView.as_view(), name='blocks_admin'),
-    path(
-        'admin/blocks/<pk>',
-        AdminBlockDetailView.as_view(),
-        name='blocks_admin_detail'),
-
+        '',
+        LocationsListCreateDestroyView.as_view(),
+        name='locations_list_create'),
     path(
         '<pk>',
-        LocationDetailsAppUserAccess.as_view(),
-        name='locations_detail_app_user_access'),
+        LocationsRetrieveUpdateDestroyView.as_view(),
+        name='locations_rud'),
 ]
