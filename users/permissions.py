5c6d6175ebe8195572313cfe708da67f65dca2ef
Saifullah <saifullah3396@gmail.com>
2021-01-30 20:46:51 +0500

    Changed permissions structure for users. Now it is handled according to group permissions.

--- users/permissions.py
+++ users/permissions.py
@@ -1,32 +1,28 @@
 # permissions.py
 from django.contrib.auth.models import Group
+from common.permissions import UserGroups, AppDjangoModelPermissions
 from rest_framework import permissions
-from backend.permissions import HasGroupPermission
+from .models import AppUser
+
+USER_GROUP_PERMISSIONS = {
+    UserGroups.ORGANIZATION_ADMIN_GROUP.name: {
+        AppUser: ['add', 'change', 'view', 'delete'],
+    },
+    UserGroups.EMPLOYEE_GROUP.name: {
+        AppUser: ['view'],
+    },
+}
 
 
-class CanRegisterUser(HasGroupPermission):
-    """
-    Ensure user is in the organization_admin or sub_organization_admin group
-    """
-    required_groups_mapping = {
-        'GET': ['organization_admin', 'sub_organization_admin'],
-        'POST': ['organization_admin', 'sub_organization_admin'],
-        'PUT': ['organization_admin', 'sub_organization_admin'],
-    }
+class CanRegisterUser(AppDjangoModelPermissions):
 
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
 
 
-class CanAccessUser(HasGroupPermission):
-    """
-    Ensure user is in the organization_admin or sub_organization_admin group
-    """
-    required_groups_mapping = {
-        'GET': ['organization_admin', 'sub_organization_admin'],
-        'POST': ['organization_admin', 'sub_organization_admin'],
-        'PUT': ['organization_admin', 'sub_organization_admin'],
-    }
+class CanAccessUser(AppDjangoModelPermissions):
 
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
