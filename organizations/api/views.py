854886373e6fc29ec81715ba260656e8811be5cb
Saifullah <saifullah3396@gmail.com>
2021-01-30 19:46:54 +0500

    Removed organization and updated views to handle api calls in the new manner, that is, by using tree structure of the model to access lower and upper level organizations based on user role.

--- organizations/api/views.py
+++ organizations/api/views.py
@@ -1,17 +1,20 @@
-from rest_framework import permissions, pagination, filters, status
+from django.contrib.auth.models import Group
+from rest_framework import \
+    mixins, permissions, pagination, filters, status, exceptions
 from rest_framework_jwt import authentication
-from rest_framework.generics import \
-    GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, \
+from rest_framework.generics import (
+    GenericAPIView,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
     DestroyAPIView
+)
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-from ..models import Organization, SubOrganization
-from .serializers import \
-    OrganizationSerializer, SubOrganizationSerializer
-from ..permissions import (
-    OrganizationsRUDPermissions,
-    SubOrganizationsListCreatePermissions,
-    SubOrganizationsRUDPermissions)
+from ..models import Organization
+from ..permissions import *
+from .serializers import OrganizationSerializer
+from backend.permissions import HasGroupPermission
+from common.utils import *
 
 
 class PaginationConfig(pagination.PageNumberPagination):
@@ -20,170 +23,133 @@ class PaginationConfig(pagination.PageNumberPagination):
     max_page_size = 100
 
 
-class OrganizationsListCreateView(ListCreateAPIView, DestroyAPIView):
-    queryset = Organization.objects.all().order_by('name')
+class OrganizationsListCreateDestroyView(ListCreateAPIView, DestroyAPIView):
+    queryset = Organization.objects.none()  # Added for model permissions
     serializer_class = OrganizationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated, permissions.IsAdminUser)
-    authentication_classes = [authentication.JSONWebTokenAuthentication]
+    permission_classes = (OrganizationListCreateDestroyPermission,)
     pagination_class = PaginationConfig
     filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     ordering_fields = ['id', 'name', 'desc']
     filterset_fields = {
         'name': ['exact', 'icontains'],
     }
+    method_to_tree_map = {
+        'GET': lambda organization: organization.get_descendants(
+            include_self=True),
+        'DELETE': lambda organization: organization.get_descendants()
+    }
 
     def get_queryset(self):
-        id_list = self.request.query_params.getlist('id')
-        if id_list:
-            return Organization.objects.filter(id__in=id_list).order_by('name')
-        return Organization.objects.all().order_by('name')
+        return self._get_queryset().order_by('name')
 
-    def delete(self, request, *args, **kwargs):
-        self.get_queryset().delete()
+    def _get_queryset(self):
+        user = self.request.user
+        if self.request.method == "GET":
+            id_list = self.request.query_params.getlist('id')
+        else:
+            id_list = self.request.data.get('id')
+
+        if user.is_staff:
+            # return complete list if request user is a staff
+            if id_list:
+                return Organization.objects.filter(id__in=id_list)
+            return Organization.objects.all()
+        else:
+            if is_organization_admin(user):
+                organizations_tree = \
+                    self.method_to_tree_map[self.request.method](
+                        user.organization)
+
+                # get the organization tree of the user if its an admin
+                if id_list:
+                    ids_in_tree = organizations_tree.filter(id__in=id_list)
+
+                    # make sure all the given ids are inside the user
+                    # descendents, otherwise return permission denied
+                    if len(ids_in_tree) != len(id_list):
+                        raise exceptions.PermissionDenied()
+                    else:
+                        return ids_in_tree
+
+                return organizations_tree
+            else:  # any other group cannot access lists
+                raise exceptions.PermissionDenied()
+
+    def perform_create(self, serializer):
+        if self.request.user.is_staff:
+            # okay to create anything for staff
+            return \
+                super(OrganizationsListCreateDestroyView, self).perform_create(
+                    serializer)
+        else:
+            # an organization cannot be created by any user other than
+            # super admin without a parent id
+            if 'parent' not in self.request.data:
+                raise exceptions.PermissionDenied()
+
+            # see if the parent of requested organization is within descendents
+            # of the current user. Current user can only be organization admin
+            # as defined by group permissions so this should work
+            descendents = self.request.user.organization.get_descendants(
+                include_self=True)
+            if not descendents.filter(id=self.request.data['parent']):
+                raise exceptions.PermissionDenied()
+
+            return \
+                super(OrganizationsListCreateDestroyView, self).perform_create(
+                    serializer)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_queryset()
+        self.perform_destroy(instance)
         return Response(data={
-            "msg": "Organizations deleted successfully."},
+            "msg": "Object(s) deleted successfully."},
             status=status.HTTP_200_OK)
 
 
-class OrganizationsRUDView(RetrieveUpdateDestroyAPIView):
+class OrganizationsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Organization.objects.none()  # Added for model permissions
     serializer_class = OrganizationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-        OrganizationsRUDPermissions)
-    authentication_classes = [authentication.JSONWebTokenAuthentication]
+    permission_classes = (OrganizationRetrieveUpdateDestroyPermission,)
     pagination_class = PaginationConfig
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['id', 'name', 'desc']
     filterset_fields = {
         'name': ['exact', 'icontains'],
     }
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_staff:
-            # return all if it is super admin
-            queryset = \
-                Organization.objects.all().order_by('name')
-            return queryset
-        else:
-            # else can only be an organization admin according to permissions
-            # on the view. Return all sub-organizations under the users
-            # organization
-            queryset = \
-                Organization.objects.filter(
-                    id=user.organization.id).order_by('name')
-            return queryset
-
-    def delete(self, request, *args, **kwargs):
-        # update delete response
-        try:
-            organization_name = self.get_object().name
-            resp = super(OrganizationsRUDView, self).delete(
-                request, *args, **kwargs)
-            return Response(data={
-                "msg": "Organization {} deleted successfully.".format(
-                    organization_name)}, status=status.HTTP_200_OK)
-        except Organization.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-class SubOrganizationsListCreateView(ListCreateAPIView, DestroyAPIView):
-    serializer_class = SubOrganizationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-        SubOrganizationsListCreatePermissions)
-    authentication_classes = [authentication.JSONWebTokenAuthentication]
-    pagination_class = PaginationConfig
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ['id', 'name', 'desc', 'organization']
-    filterset_fields = {
-        'name': ['exact', 'icontains'],
-        'organization': ['exact']
-    }
-
-    def send_user_response(self, request, user):
-        serializer = self.serializer_class(
-            user, context={'request': request})
-        return Response({"user": serializer.data}, status=status.HTTP_200_OK)
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_staff:
-            # return all if it is super admin
-            queryset = SubOrganization.objects.all().order_by('name')
-            return queryset
-        else:
-            # else can only be an organization admin according to permissions
-            # on the view. Return all sub-organizations under the users
-            # organization
-            queryset = SubOrganization.objects.filter(
-                organization__id=user.organization.id).order_by('name')
-            return queryset
-
-    def create(self, request, *args, **kwargs):
-        # check that the user is admin of the organization in which
-        # sub-organization is requested
-        if request.user.is_staff or \
-                request.data['organization'] == \
-                str(request.user.organization.id):
-            return super(SubOrganizationsListCreateView, self).create(
-                request, *args, *kwargs)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-    def delete(self, request, *args, **kwargs):
-        if request.user.is_staff or \
-                request.data['organization'] == \
-                str(request.user.organization.id):
-            self.get_queryset().delete()
-            return Response(data={
-                "msg": "Organizations deleted successfully."},
-                status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-
-
-class SubOrganizationsRUDView(RetrieveUpdateDestroyAPIView):
-    serializer_class = SubOrganizationSerializer
-    permission_classes = (
-        permissions.IsAuthenticated,
-        SubOrganizationsRUDPermissions)
-    authentication_classes = [authentication.JSONWebTokenAuthentication]
-    pagination_class = PaginationConfig
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ['id', 'name', 'desc', 'organization']
-    filterset_fields = {
-        'name': ['exact', 'icontains'],
-        'organization': ['exact']
+    method_to_tree_map = {
+        'GET': lambda organization: organization.get_descendants(
+            include_self=True),
+        'POST': lambda organization: organization.get_descendants(
+            include_self=True),
+        'PATCH': lambda organization: organization.get_descendants(
+            include_self=True),
+        'DELETE': lambda organization: organization.get_descendants()
     }
 
     def get_queryset(self):
+        return self._get_queryset().order_by('name')
+
+    def _get_queryset(self):
         user = self.request.user
         if user.is_staff:
-            # return all if it is super admin
-            queryset = SubOrganization.objects.all().order_by('name')
-            return queryset
+            # return complete list if request user is a staff
+            return Organization.objects.all()
         else:
-            if user.sub_organization is None:
-                # user is organization admin so return all underlying sub-
-                # organizations
-                queryset = SubOrganization.objects.filter(
-                    organization__id=user.organization.id).order_by('name')
+            if is_organization_admin(user):
+                organizations_tree = \
+                    self.method_to_tree_map[self.request.method](
+                        user.organization)
+                return organizations_tree
             else:
-                # Return only the associated sub organization
-                queryset = SubOrganization.objects.filter(
-                    id=user.sub_organization.id).order_by('name')
-            return queryset
+                # any other group can only access object if its in the same
+                # organization
+                return Organization.objects.filter(
+                    id=user.organization.id)
 
-    def delete(self, request, *args, **kwargs):
-        # update delete response
-        try:
-            sub_organization_name = self.get_object().name
-            resp = super(SubOrganizationsRUDView, self).delete(
-                request, *args, **kwargs)
-            return Response(data={
-                "msg": "Sub-organization {} deleted successfully.".format(
-                    sub_organization_name)}, status=status.HTTP_200_OK)
-        except SubOrganization.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def destroy(self, request, *args, **kwargs):
+        resp = super(OrganizationsRetrieveUpdateDestroyView, self).destroy(
+            request, *args, **kwargs)
+        return Response(data={
+            "msg": "Object(s) deleted successfully."},
+            status=status.HTTP_200_OK)
