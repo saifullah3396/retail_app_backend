27e2ef8a285819ef139d2bbe410a21572d005ea1
saifullah3396 <Saifullah3396@gmail.com>
2021-02-01 15:44:27 +0500

    Added locations api views for list,create,update,delete,etc.

--- locations/api/views.py
+++ locations/api/views.py
@@ -27,11 +27,40 @@ class LocationsListCreateDestroyView(ListCreateAPIView, DestroyAPIView):
     filterset_fields = {
         'name': ['exact', 'icontains'],
     }
-    method_to_tree_map = {
+    organizations_tree_map = {
         'GET': lambda organization: organization.get_descendants(
             include_self=True),
-        'DELETE': lambda organization: organization.get_descendants()
+        'DELETE': lambda organization: organization.get_descendants(
+            include_self=True)
     }
+
+    def get_organizations_tree(self, organization):
+        return self.organizations_tree_map[self.request.method](organization)
+
+    def get_locations_in_organizations(self, organizations):
+        return Location.objects.filter(organization__in=organizations)
+
+    def filter_queryset_by_id_list(self, query_set, id_list):
+        return query_set.filter(id__in=id_list)
+
+    def exclude_queryset_by_id_list(self, query_set, id_list):
+        return query_set.exclude(id__in=id_list)
+
+    def filter_locations_by_id_list(self, locations, id_list):
+        # filter locations by id list
+        filtered_locations = self.filter_queryset_by_id_list(
+            locations, id_list)
+
+        # make sure all the given ids are inside filtered locations,
+        # otherwise raise a validation error
+        if len(filtered_locations) != len(id_list):
+            raise exceptions.NotFound(
+                {
+                    'id': 'The following requested ids are invalid: {}'.format(
+                        self.exclude_queryset_by_id_list(
+                            locations, id_list).values_list('id', flat=True))
+                })
+        return filtered_locations
 
     def get_queryset(self):
         return self._get_queryset().order_by('name')
@@ -44,67 +73,64 @@ class LocationsListCreateDestroyView(ListCreateAPIView, DestroyAPIView):
             id_list = self.request.data.get('id')
 
         if user.is_staff:
-            print('user.is_staff')
             # return complete list if request user is a staff
             if id_list:
-                return Location.objects.filter(id__in=id_list)
+                return self.filter_queryset_by_id_list(
+                    Location.objects, id_list)
             return Location.objects.all()
+        elif is_organization_admin(user):
+            # get all organizations under this one
+            organizations_tree = self.get_organizations_tree(
+                user.organization)
+
+            # get all locations in the tree
+            locations_in_tree = self.get_locations_in_organizations(
+                organizations_tree)
+
+            if id_list:
+                return self.filter_locations_by_id_list(
+                    locations_in_tree, id_list)
+
+            return locations_in_tree
         else:
-            print(user.groups.all())
-            if is_organization_admin(user):
-                print('is_organization_admin')
-                organizations_tree = \
-                    self.method_to_tree_map[self.request.method](
-                        user.organization)
-
-                locations_in_tree = \
-                    Location.objects.filter(
-                        organization__in=organizations_tree)
-
-                print('org tree', organizations_tree)
-                print('locations_in_tree', locations_in_tree)
-
-                # get the organization tree of the user if its an admin
-                if id_list:
-                    ids_in_tree = locations_in_tree.filter(id__in=id_list)
-
-                    # make sure all the given ids are inside the user
-                    # descendents, otherwise return permission denied
-                    if len(ids_in_tree) != len(id_list):
-                        raise exceptions.PermissionDenied()
-                    else:
-                        return ids_in_tree
-
-                return locations_in_tree
-            else:  # any other group can only get locations that are authorized
-                return user.authorized_locations.all()
+            # any other group can only get locations that are authorized
+            authorized_locations = user.authorized_locations.all()
+            if id_list:
+                return self.filter_locations_by_id_list(
+                    authorized_locations, id_list)
+            return authorized_locations
 
     def perform_create(self, serializer):
-        if self.request.user.is_staff:
+        user = self.request.user
+        data = self.request.data
+
+        if user.is_staff:
             # okay to create anything for staff
             return \
-                super(LocationListCreateDestroyView, self).perform_create(
+                super(LocationsListCreateDestroyView, self).perform_create(
                     serializer)
-        else:
-            # an organization cannot be created by any user other than
-            # super admin without a parent id
-            if 'parent' not in self.request.data:
-                raise exceptions.PermissionDenied()
-
-            # see if the parent of requested organization is within descendents
-            # of the current user. Current user can only be organization admin
-            # as defined by group permissions so this should work
-            descendents = self.request.user.organization.get_descendants(
+        elif is_organization_admin(user):
+            # see if the organization of requested location is within
+            # descendents of this admin
+            descendents = user.organization.get_descendants(
                 include_self=True)
-            if not descendents.filter(id=self.request.data['parent']):
-                raise exceptions.PermissionDenied()
+            if not descendents.filter(id=data['organization']):
+                raise exceptions.ValidationError(
+                    {
+                        'organization': 'Invalid value.'
+                    })
 
             return \
-                super(LocationListCreateDestroyView, self).perform_create(
+                super(LocationsListCreateDestroyView, self).perform_create(
                     serializer)
+        else:
+            # no other user group can create a location
+            raise exceptions.PermissionDenied()
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_queryset()
+        if not instance:
+            raise exceptions.NotFound()
         self.perform_destroy(instance)
         return Response(data={
             "msg": "Object(s) deleted successfully."},
@@ -121,15 +147,24 @@ class LocationsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     filterset_fields = {
         'name': ['exact', 'icontains'],
     }
-    method_to_tree_map = {
+
+    organizations_tree_map = {
         'GET': lambda organization: organization.get_descendants(
             include_self=True),
         'POST': lambda organization: organization.get_descendants(
             include_self=True),
         'PATCH': lambda organization: organization.get_descendants(
             include_self=True),
-        'DELETE': lambda organization: organization.get_descendants()
+        'DELETE': lambda organization: organization.get_descendants(
+            include_self=True
+        )
     }
+
+    def get_organizations_tree(self, organization):
+        return self.organizations_tree_map[self.request.method](organization)
+
+    def get_locations_in_organizations(self, organizations):
+        return Location.objects.filter(organization__in=organizations)
 
     def get_queryset(self):
         return self._get_queryset().order_by('name')
@@ -139,20 +174,22 @@ class LocationsRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
         if user.is_staff:
             # return complete list if request user is a staff
             return Location.objects.all()
+        elif is_organization_admin(user):
+            # get all organizations under this one
+            organizations_tree = self.get_organizations_tree(
+                user.organization)
+
+            # get all locations in the tree
+            locations_in_tree = self.get_locations_in_organizations(
+                organizations_tree)
+            return locations_in_tree
         else:
-            if is_organization_admin(user):
-                organizations_tree = \
-                    self.method_to_tree_map[self.request.method](
-                        user.organization)
-                locations_in_tree = \
-                    Location.objects.filter(
-                        organization__in=organizations_tree)
-                return locations_in_tree
-            else:  # any other group can only get locations that are authorized
-                return user.authorized_locations.all()
+            # any other group can only get locations that are authorized
+            authorized_locations = user.authorized_locations.all()
+            return authorized_locations
 
     def destroy(self, request, *args, **kwargs):
-        resp = super(LocationRetrieveUpdateDestroyView, self).destroy(
+        resp = super(LocationsRetrieveUpdateDestroyView, self).destroy(
             request, *args, **kwargs)
         return Response(data={
             "msg": "Object(s) deleted successfully."},
