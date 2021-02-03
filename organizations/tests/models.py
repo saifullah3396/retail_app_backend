da2fecb4706370b4297d12ebf30dfd7c8a61bc6a
saifullah3396 <Saifullah3396@gmail.com>
2021-02-03 15:44:59 +0500

    Updated models test.

--- organizations/tests/models.py
+++ organizations/tests/models.py
@@ -15,7 +15,21 @@ class OrganizationModelTests(TestCase):
             desc='NUST University Description'
         )
 
+        self.sub_organization = Organization.objects.create(
+            name='SMME',
+            desc='SMME School Description',
+            parent=self.organization
+        )
+
     def test_model(self):
+        # test details of the organization
         organization = Organization.objects.get(id=self.organization.id)
         self.assertEquals(organization.name, 'NUST')
         self.assertEquals(organization.desc, 'NUST University Description')
+
+        # test details of the sub-organization made under organization
+        sub_organization = Organization.objects.get(
+            id=self.sub_organization.id)
+        self.assertEquals(sub_organization.name, 'SMME')
+        self.assertEquals(sub_organization.desc, 'SMME School Description')
+        self.assertEquals(sub_organization.parent.id, organization.id)
