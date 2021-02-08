4eb886655618e835aea51c73b026c2d702cb70d3
saifullah3396 <Saifullah3396@gmail.com>
2021-02-08 15:06:48 +0500

    Updated serializers for floor, block, and location listing and details.

--- locations/api/serializers.py
+++ locations/api/serializers.py
@@ -7,28 +7,28 @@ from rest_framework import serializers
 from ..models import Block, Floor, Location
 
 
-class LocationSerializer(serializers.ModelSerializer):
+class LocationListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
 
 
-class FloorSerializer(serializers.ModelSerializer):
+class FloorListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Floor
-        fields = '__all__'
+        fields = ('id', 'number')
 
 
-class BlockSerializer(serializers.ModelSerializer):
+class BlockListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Block
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
-class BlockDetailSerializer(BlockSerializer):
+class BlockDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Block
-        fields = '__all__'
+        fields = ('id', 'name', 'floor_map', 'coordinate_frame')
 
 
 class FloorDetailSerializer(serializers.ModelSerializer):
@@ -42,26 +42,6 @@ class FloorDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Floor
-        fields = ('id', 'number', 'location', 'blocks')
-
-
-class BlockInLocationDetailSerializer(BlockSerializer):
-    class Meta:
-        model = Block
-        fields = ('id', 'name', 'floor_map', 'coordinate_frame')
-
-
-class FloorInLocationDetailSerializer(serializers.ModelSerializer):
-    blocks = serializers.SerializerMethodField()
-
-    def get_blocks(self, floor):
-        # return all blocks in this floor
-        blocks = Block.objects.filter(floor=floor)
-        return \
-            BlockInLocationDetailSerializer(blocks, many=True).data
-
-    class Meta:
-        model = Floor
         fields = ('id', 'number', 'blocks')
 
 
@@ -72,7 +52,7 @@ class LocationDetailSerializer(serializers.ModelSerializer):
         # return all floors in this location
         floors = Floor.objects.filter(location__id=location.id)
         return \
-            FloorInLocationDetailSerializer(floors, many=True).data
+            FloorDetailSerializer(floors, many=True).data
 
     class Meta:
         model = Location
