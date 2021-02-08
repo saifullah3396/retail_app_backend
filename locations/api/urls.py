788e7a5dfabc6cfdab37dd1712008b43736325bf
saifullah3396 <Saifullah3396@gmail.com>
2021-02-08 15:07:17 +0500

    Added new urls for floors and blocks fetching based on location and floor respectively.

--- locations/api/urls.py
+++ locations/api/urls.py
@@ -4,7 +4,9 @@ Defines the urls for the views defined in the locations api.
 
 from django.urls import path
 
-from .views import *
+from .views.locations_views import *
+from .views.floors_views import *
+from .views.blocks_views import *
 
 urlpatterns = [
     path(
@@ -15,4 +17,20 @@ urlpatterns = [
         '<pk>',
         LocationsRetrieveUpdateDestroyView.as_view(),
         name='locations_retrieve_update_delete'),
+    path(
+        '<uuid:location_id>/floors/',
+        FloorsListCreateDestroyView.as_view(),
+        name='floors_list_create_delete'),
+    path(
+        '<uuid:location_id>/floors/<pk>',
+        FloorsRetrieveUpdateDestroyView.as_view(),
+        name='floors_retrieve_update_delete'),
+    path(
+        '<uuid:location_id>/floors/<uuid:floor_id>/blocks/',
+        BlocksListCreateDestroyView.as_view(),
+        name='blocks_list_create_delete'),
+    path(
+        '<uuid:location_id>/floors/<uuid:floor_id>/blocks/<pk>',
+        BlocksRetrieveUpdateDestroyView.as_view(),
+        name='blocks_retrieve_update_delete'),
 ]
