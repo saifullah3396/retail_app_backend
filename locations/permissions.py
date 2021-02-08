4871f4c27d660d00ed415551cd1ae58fe2bba663
saifullah3396 <Saifullah3396@gmail.com>
2021-02-08 15:06:14 +0500

    Added new permissions for floors and blocks.

--- locations/permissions.py
+++ locations/permissions.py
@@ -6,17 +6,21 @@ import copy
 
 from core.permissions import AppDjangoModelPermissions, UserGroups
 
-from .models import Location
+from .models import Location, Floor, Block
 
 """
-Define the user group permissions for the Locations model.
+Define the user group permissions for the models of this application.
 """
 USER_GROUP_PERMISSIONS = {
     UserGroups.ORGANIZATION_ADMIN_GROUP.name: {
         Location: ['add', 'change', 'view', 'delete'],
+        Floor: ['add', 'change', 'view', 'delete'],
+        Block: ['add', 'change', 'view', 'delete']
     },
     UserGroups.EMPLOYEE_GROUP.name: {
         Location: ['view'],
+        Floor: ['view'],
+        Block: ['view']
     },
 }
 
@@ -34,6 +38,46 @@ class LocationsListCreateDestroyPermission(AppDjangoModelPermissions):
 class LocationsRetrieveUpdateDestroyPermission(AppDjangoModelPermissions):
     """
     Permissions required on the LocationsRetrieveUpdateDestroyView
+    """
+
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class FloorsListCreateDestroyPermission(AppDjangoModelPermissions):
+    """
+    Permissions required on the FloorsListCreateDestroyView
+    """
+
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class FloorsRetrieveUpdateDestroyPermission(AppDjangoModelPermissions):
+    """
+    Permissions required on the FloorsRetrieveUpdateDestroyView
+    """
+
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class BlocksListCreateDestroyPermission(AppDjangoModelPermissions):
+    """
+    Permissions required on the BlocksListCreateDestroyView
+    """
+
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class BlocksRetrieveUpdateDestroyPermission(AppDjangoModelPermissions):
+    """
+    Permissions required on the BlocksRetrieveUpdateDestroyView
     """
 
     def __init__(self):
