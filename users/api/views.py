3673cb8b4e0768886609e6cbb6fcaa9b99cd2537
saifullah3396 <Saifullah3396@gmail.com>
2021-02-12 14:38:22 +0500

    App users now has the list-create-delete api view the same as other apps inheriting from core. Although creation is not supported here as user must register through django-rest-auth for that.

--- users/api/views.py
+++ users/api/views.py
@@ -1,16 +1,139 @@
-from rest_framework import permissions, status
-from rest_framework_jwt import authentication
-from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from core.utils import *
+from core.views import *
+from django.contrib.auth.models import Group
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import *
+from rest_framework import filters, pagination
+from rest_framework.generics import *
 from rest_framework.response import Response
+from rest_framework_jwt import authentication
+
 from ..models import AppUser
-from .serializers import \
-    AdminUserSerializerAdminAccess, \
-    AppUserSerializerAdminAccess, \
-    AppUserSerializerAppUserAccess
+from ..permissions import *
+from .serializers import *
 
 
-class AppUserListAdminAccess(ListAPIView):
+class AppUsersView:
+    ordering_fields = ['id', 'username']
+    filterset_fields = {
+        'username': ['exact', 'icontains'],
+    }
+
+    def _get_model(self):
+        """
+        Returns the get queryset for staff users.
+        """
+
+        return AppUser
+
+    def _order_by(self):
+        """
+        Returns the field with respect to which queries are to be ordered.
+        """
+        return 'first_name'
+
+    def _get_organizations_tree(self, organization):
+        """
+        Returns the organizations descendents tree given the request type and
+        organzation.
+        """
+        return self.organizations_tree_wrt_request[self.request.method](
+            organization)
+
+    def _get_users_in_organizations(self, organizations):
+        """
+        Returns all users which present within the given organizations
+        queryset
+        """
+        return AppUser.objects.filter(organization__in=organizations)
+
+
+class AppUsersListCreateDestroyView(
+        CoreListCreateDestroyView, AppUsersView):
+    """
+    Defines the organizations list-create-destroy view.
+    """
+
+    queryset = AppUser.objects.none()
+    serializer_class = AppUserListSerializer
+    permission_classes = (AppUsersListCreateDestroyPermission,)
+
+    # Define the mapping from request type to query that returns the
+    # organizations tree that is used in the requests. For example, in DELETE
+    # requests, organization does not include itself while in GET it does
+    organizations_tree_wrt_request = {
+        'GET': lambda organization: organization.get_descendants(
+            include_self=True),
+        'DELETE': lambda organization: organization.get_descendants()
+    }
+
+    def _get_model(self):
+        """
+        Returns the get queryset for staff users.
+        """
+
+        return AppUsersView._get_model(self)
+
+    def _order_by(self):
+        """
+        Returns the field with respect to which queries are to be ordered.
+        """
+        return AppUsersView._order_by(self)
+
+    def _define_get_queryset_by_group_fn(self):
+        """
+        Returns a dictionary mapping user group to get_queryset function
+        that will be called if the request user is in that user group.
+        """
+        return {
+            UserGroups.ORGANIZATION_ADMIN_GROUP:
+                self._get_organization_admin_queryset
+        }
+
+    def _define_perform_create_by_group_fn(self):
+        """
+        Returns a dictionary mapping user group to perform_create function
+        that will be called if the request user is in that user group.
+        """
+        return {}
+
+    def _get_organization_admin_queryset(self):
+        """
+        For an organization admin, all the users in the organizations below
+        organization are returned. In case a list of ids is provided, the
+        users are filtered further by ids.
+        """
+        print("_get_organization_admin_queryset")
+        id_list = self._get_id_list()
+        organizations_tree = self._get_organizations_tree(
+            self.request.user.organization)
+
+        # get all users in the organizations tree
+        users_in_tree = self._get_users_in_organizations(
+            organizations_tree)
+
+        id_list = self._get_id_list()
+        if id_list:
+            return self._filter_objects_by_id_list(
+                users_in_tree, id_list)
+
+        return users_in_tree
+
+        # get the organization tree of the user if its an admin
+        if id_list:
+            return self._filter_objects_by_id_list(
+                organizations_tree, id_list
+            )
+        return organizations_tree
+
+    def perform_create(self, serializer):
+        """
+        User creation is done through RegisterView from django rest-auth
+        """
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
     """
     Lists all app users. Can only be accessed by staff users.
     """
