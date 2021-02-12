a35168adce306e5cfc49af2a01b68aa20692b98f
saifullah3396 <Saifullah3396@gmail.com>
2021-02-12 12:58:57 +0500

    Added read/write serailizer method field to the utils.

--- core/utils.py
+++ core/utils.py
@@ -3,9 +3,32 @@ Defines the common utility functions used in our applications.
 """
 
 from django.contrib.auth.models import Group
-from rest_framework import exceptions
+from rest_framework import exceptions, serializers
 
 from core.permissions import UserGroups
+
+
+class WritableSerializerMethodField(serializers.SerializerMethodField):
+
+    def __init__(self, method_name=None, **kwargs):
+        self.method_name = method_name
+        self.setter_method_name = kwargs.pop('setter_method_name', None)
+        self.deserializer_field = kwargs.pop('deserializer_field')
+
+        kwargs['source'] = '*'
+        super(serializers.SerializerMethodField, self).__init__(**kwargs)
+
+    def bind(self, field_name, parent):
+        retval = super().bind(field_name, parent)
+        if not self.setter_method_name:
+            self.setter_method_name = f'set_{field_name}'
+
+        return retval
+
+    def to_internal_value(self, data):
+        value = self.deserializer_field.to_internal_value(data)
+        method = getattr(self.parent, self.setter_method_name)
+        return method(value)
 
 
 def get_user_from_serializer(serializer, raise_exception=False):
