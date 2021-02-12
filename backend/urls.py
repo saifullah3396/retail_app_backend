19ffa9b5e7e97b558fac1dfd9b73afebb5a68e4a
saifullah3396 <Saifullah3396@gmail.com>
2021-02-12 14:50:25 +0500

    Added django rest auth urls.

--- backend/urls.py
+++ backend/urls.py
@@ -1,19 +1,20 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework_jwt.views import obtain_jwt_token
-from .settings import MEDIA_URL, MEDIA_ROOT
+
+from .settings import MEDIA_ROOT, MEDIA_URL
 
 api_urlpatterns = [
     path('organizations/', include('organizations.api.urls')),
     path('locations/', include('locations.api.urls')),
     path('cameras/', include('cameras.api.urls')),
     path('users/', include('users.api.urls')),
+    path('user_auth/', include('user_auth.urls')),
 ]
 
 urlpatterns = [
-    path('accounts/', include('rest_auth.urls')),
-    path('accounts/registration/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_urlpatterns)),
     path('api-token-auth/', obtain_jwt_token),
