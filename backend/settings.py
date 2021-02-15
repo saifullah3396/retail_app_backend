d17546c5333e476c8224d9a97889a888d0b2be03
saifullah3396 <Saifullah3396@gmail.com>
2021-02-15 16:14:49 +0500

    Added environment config schema for local testing of django apps.

--- backend/settings.py
+++ backend/settings.py
@@ -13,10 +13,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_URL = "http://127.0.0.1/"
+BASE_URL = "http://0.0.0.0/"
+
+# load environment variables stored in .env
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -118,6 +122,7 @@ DATABASES = {
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # User config
 AUTH_USER_MODEL = "users.AppUser"
