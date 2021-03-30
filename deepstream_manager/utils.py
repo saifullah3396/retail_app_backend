b17e5a61a3ad583403780a454036766011503481
saifullah3396 <Saifullah3396@gmail.com>
2021-03-30 14:17:00 +0500

    Added messaging protocols for communication between server and frontend clients and server and deepstream servers.

---
+++ deepstream_manager/utils.py
@@ -0,0 +1,18 @@
+"""
+Defines the utilities used in websocket send/recieve operations.
+"""
+from enum import IntEnum
+
+
+class DeepstreamFrontendStreamerCommands(IntEnum):
+    STOP_STREAMING = 0
+    START_STREAMING = 1
+    CHANGE_CAMERA_IDS = 2
+
+
+class DeepstreamBackendStreamerCommands(IntEnum):
+    SEND_ADDR = 0
+    SEND_DIAGNOSTICS = 1
+    UPDATE_CONFIG = 2
+    STOP_STREAMING = 3
+    START_STREAMING = 4
