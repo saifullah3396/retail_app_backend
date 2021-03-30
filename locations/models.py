3c8f26ffc6edbe3b1c37b124762c1eb0c7e1cd19
saifullah3396 <Saifullah3396@gmail.com>
2021-03-30 14:10:43 +0500

    Added block size to model.

--- locations/models.py
+++ locations/models.py
@@ -4,9 +4,9 @@ Defines the models of this application.
 
 import uuid
 
+from django.db import models
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
-from django.db import models
 
 
 class Location(models.Model):
