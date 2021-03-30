87443f6c4b619b5d86637a18406a5f5477f6c265
saifullah3396 <Saifullah3396@gmail.com>
2021-03-30 14:18:26 +0500

    Added routes for front/back/end streamers.

---
+++ deepstream_manager/routing.py
@@ -0,0 +1,28 @@
+"""
+Defines the routing for the consumers
+"""
+
+from channels.http import AsgiHandler
+from django.conf.urls import url
+from django.core.asgi import get_asgi_application
+from django.urls import re_path
+
+from . import deepstream_live_receiver, deepstream_live_sender
+
+websocket_urlpatterns = [
+    re_path(
+        r'^deepstream/client/(?P<group_id>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$',
+        deepstream_live_sender.DeepstreamFrontendStreamer.as_asgi()),
+    re_path(
+        # r'^deepstream/server/(?P<group_id>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$',
+        r'^deepstream/server/',
+        deepstream_live_receiver.DeepstreamBackendStreamer.as_asgi()),
+]
+
+http_urlpatterns = [
+    # re_path(
+    #     # r'^deepstream/server/(?P<group_id>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$',
+    #     r'^deepstream/server/',
+    #     deepstream_live_receiver.DeepstreamBackendStreamer.as_asgi()),
+    url(r'', get_asgi_application()),
+]
