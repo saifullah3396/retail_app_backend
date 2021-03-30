a15609cb4450f3e45b8733cf6e1b5ea2b19d5b71
saifullah3396 <Saifullah3396@gmail.com>
2021-03-30 14:11:40 +0500

    Added redirect view for favicon.

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from rest_framework_jwt.views import obtain_jwt_token
 
 from .settings import MEDIA_ROOT, MEDIA_URL
