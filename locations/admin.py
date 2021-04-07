0ad49ecad87793681f136c54387f48caed9e6ae0
saifullah3396 <Saifullah3396@gmail.com>
2021-04-07 12:31:03 +0500

    Added Measurement Frame to admin site.

--- locations/admin.py
+++ locations/admin.py
@@ -2,10 +2,13 @@
 Registers the models to the admin interface.
 """
 
+from django.contrib.gis.db import models as geomodels
 from django.contrib import admin
 
-from .models import Block, Floor, Location
+from .models import MeasurementFrame, Block, Floor, Location
+
 
 admin.site.register(Location)
 admin.site.register(Floor)
 admin.site.register(Block)
+admin.site.register(MeasurementFrame)
