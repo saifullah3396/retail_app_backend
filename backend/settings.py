3f8e58d21ce03e7c0a0e04594c0abc80909b1ecc
saifullah3396 <Saifullah3396@gmail.com>
2021-04-07 12:22:30 +0500

    Allowed all hosts for testing purposes.

--- backend/settings.py
+++ backend/settings.py
@@ -33,11 +33,16 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    'localhost',
-    '127.0.0.1',
-    'https://retail-irl-ncai.herokuapp.com/']
+# ALLOWED_HOSTS = [
+#     '0.0.0.0',
+#     'localhost',
+#     '127.0.0.1',
+#     'https://retail-irl-ncai.herokuapp.com/']
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+# )
+
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
