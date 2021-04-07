1268f34329ecb70887cfbd2d6df4cd0240331718
saifullah3396 <Saifullah3396@gmail.com>
2021-04-07 12:27:06 +0500

    Updated db configuration after removal of postgis.

--- backend/settings.py
+++ backend/settings.py
@@ -139,14 +139,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'retail-apps',
-        'USER': 'awais',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'retail-app',
+        'USER': 'sai',
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # User config
 AUTH_USER_MODEL = "users.AppUser"
