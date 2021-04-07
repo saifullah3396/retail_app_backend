39505fe1ed17b8508be143485d7ca0e59bd4f777
saifullah3396 <Saifullah3396@gmail.com>
2021-04-07 12:28:09 +0500

    Updated camera model with new fields for image and map point coordinates. Also added foreign keys to server, and measurement frame to which the camera is associated.

--- cameras/models.py
+++ cameras/models.py
@@ -1,9 +1,6 @@
 import uuid
 
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import Point
-
-# Create your models here.
 
 
 class Camera(models.Model):
@@ -13,28 +10,58 @@ class Camera(models.Model):
 
     """Unique uuid for each camera."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    """Place name where camera is located."""
-    place_name = models.CharField(max_length=125, default="Main", unique=True)
 
     """Camera ip Address."""
     ip_addr = models.CharField(max_length=120)
 
-    """ Camera coordinates with respect to block frame"""
-    coords = models.PointField(default=Point(0, 0))
+    """ Coordinates of the reference point p0 in frame """
+    p0_coord_in_frame_x = models.IntegerField(default=0)
+    p0_coord_in_frame_y = models.IntegerField(default=0)
+
+    """ Coordinates of the other mapping points p1, p2, p3 wrt, p0 in frame """
+    p1_coord_in_frame_x = models.IntegerField(default=0)
+    p1_coord_in_frame_y = models.IntegerField(default=0)
+    p2_coord_in_frame_x = models.IntegerField(default=0)
+    p2_coord_in_frame_y = models.IntegerField(default=0)
+    p3_coord_in_frame_x = models.IntegerField(default=0)
+    p3_coord_in_frame_y = models.IntegerField(default=0)
+
+    """ Coordinates of the all the reference points in camera image pixel
+        coordinates """
+    p0_coord_in_image_x = models.IntegerField(default=0)
+    p0_coord_in_image_y = models.IntegerField(default=0)
+    p1_coord_in_image_x = models.IntegerField(default=0)
+    p1_coord_in_image_y = models.IntegerField(default=0)
+    p2_coord_in_image_x = models.IntegerField(default=0)
+    p2_coord_in_image_y = models.IntegerField(default=0)
+    p3_coord_in_image_x = models.IntegerField(default=0)
+    p3_coord_in_image_y = models.IntegerField(default=0)
 
     """ Block name with which the camera is associated """
     block = models.ForeignKey(
         'locations.Block',
         on_delete=models.CASCADE,
     )
+
     """ Server with which the camera is associated """
-    server = models.ForeignKey(
-        'servers.Server',
+    deepstream_server = models.ForeignKey(
+        'deepstream_servers.DeepstreamServer',
         on_delete=models.CASCADE,
+    )
+
+    """ Frame with which the camera measurements are taken. """
+    measurement_frame = models.ForeignKey(
+        'locations.MeasurementFrame',
+        on_delete=models.CASCADE,
+        null=True
     )
 
     def __str__(self):
         """
         String serializer of the model
         """
-        return "Camera={}, {}".format(self.place_name, str(self.block))
+        return "Camera={}, {}, {}, {}".format(
+            self.ip_addr,
+            str(self.deepstream_server),
+            str(self.measurement_frame),
+            str(self.block))
