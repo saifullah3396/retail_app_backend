bb75309d7e926023581d46f907ce8ac9612886d5
asif ali <asif.engr.official@gmail.com>
2021-04-09 16:37:05 +0500

    Removed postgis,

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     'django_filters',
     'django_extensions',
 
@@ -142,8 +141,9 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'retail-app',
-        'USER': 'sai',
+        'NAME': 'admin_panel',
+        'USER': 'asif',
+        'PASSWORD':'EngrAsif@065'
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
--- locations/admin.py
+++ locations/admin.py
@@ -2,7 +2,6 @@
 Registers the models to the admin interface.
 """
 
-from django.contrib.gis.db import models as geomodels
 from django.contrib import admin
 
 from .models import MeasurementFrame, Block, Floor, Location
