54edcf765cb6dded6588d38e0a798bb11c572fb2
saifullah3396 <Saifullah3396@gmail.com>
2021-04-12 12:19:37 +0500

    Removed postgist.

--- backend/settings.py
+++ backend/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     'django_filters',
     'django_extensions',
 
