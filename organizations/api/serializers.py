d53e3d769c44401c0440adc20569ddb0616a1f05
saifullah3396 <saifullah3396@gmail.com>
2021-04-15 20:00:00 +0500

    Added more depth to serialized fields.

--- organizations/api/serializers.py
+++ organizations/api/serializers.py
@@ -8,11 +8,44 @@ from organizations.models import Organization
 
 
 # pylint: disable=missing-class-docstring
-class OrganizationSerializer(serializers.ModelSerializer):
+class OrganizationListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Organization
-        fields = '__all__'
+        fields = ('id', 'name', 'parent',)
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'name': {'required': True},
+        }
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        repr_data = {}
+        repr_data['id'] = data['id']
+        repr_data['name'] = data['name']
+        if data['parent']:
+            try:
+                parent_organization = \
+                    Organization.objects.get(id=data['parent'])
+                repr_data['parent'] = {
+                    'id': parent_organization.id,
+                    'name': parent_organization.name
+                }
+            except Organization.DoesNotExist:
+                return None
+        return repr_data
+
+
+class OrganizationDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Organization
+        fields = ('id', 'name', 'parent',)
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'name': {'required': True},
+            'parent': {'read_only': True},
+        }
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
