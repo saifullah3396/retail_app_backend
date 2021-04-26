c5c9d5313a8aff3e2d62a0a26d8c8bce37e87abe
saifullah3396 <Saifullah3396@gmail.com>
2021-04-26 11:47:30 +0500

    Removed default from required fields.

--- locations/models.py
+++ locations/models.py
@@ -70,15 +70,14 @@ class Block(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     """Name number."""
-    name = models.CharField(default='Main Block', max_length=150)
+    name = models.CharField(max_length=150)
 
     """Block floor map image."""
-    floor_map = models.ImageField(
-        upload_to='maps', blank=True, null=True)
+    floor_map = models.ImageField(upload_to='maps')
 
     """Pixel to meters resolution of the block from map to real world"""
-    pixels_to_m_x = models.FloatField(default=0.0)
-    pixels_to_m_y = models.FloatField(default=0.0)
+    pixels_to_m_x = models.FloatField()
+    pixels_to_m_y = models.FloatField()
 
     """Floor with which this block is associated."""
     floor = models.ForeignKey(
