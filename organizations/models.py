28ec14c825a47e5eff95ca4c4ac8432112864dad
saifullah3396 <Saifullah3396@gmail.com>
2021-04-30 09:18:37 +0500

    Added uniqueness constraints to model.

--- organizations/models.py
+++ organizations/models.py
@@ -5,6 +5,8 @@ Defines the model of an organization
 import uuid
 
 from django.db import models
+from django.db.models import Q
+from django.db.models.constraints import UniqueConstraint
 from mptt.models import MPTTModel, TreeForeignKey
 
 
@@ -18,11 +20,21 @@ class Organization(MPTTModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     """Unique organization name."""
-    name = models.CharField(max_length=125, default="Unknown", unique=True)
+    name = models.CharField(max_length=125)
 
     """Parent organization if any exists."""
     parent = TreeForeignKey('self', on_delete=models.PROTECT,
                             null=True, blank=True, related_name='children')
+
+    class Meta:
+        """Don't allow non-unique names for any given parent."""
+        constraints = [
+            UniqueConstraint(fields=['name', 'parent'],
+                             name='unique_with_parent'),
+            UniqueConstraint(fields=['name'],
+                             condition=Q(parent=None),
+                             name='unique_without_parent'),
+        ]
 
     def __str__(self):
         """
